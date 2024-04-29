ac4bfd4904a444538432ad46c1808073821c466e
Tian Gao <gaogaotiantian@hotmail.com>
2024-04-28 20:19:54 -0700

    Ready for 0.1.0

--- src/coredumpy/__init__.py
+++ src/coredumpy/__init__.py
@@ -2,7 +2,7 @@
 # For details: https://github.com/gaogaotiantian/coredumpy/blob/master/NOTICE.txt
 
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 import coredumpy.pytest_hook as pytest_hook
 from .coredumpy import Coredumpy, dump, load
