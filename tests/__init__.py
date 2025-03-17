ef0bd470d17337d8d311783100b597ef39cb9bcb
gaogaotiantian <gaogaotiantian@hotmail.com>
2025-03-17 15:11:17 -0400

    Revert coredumpy hook

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,2 @@
 # Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
 # For details: https://github.com/gaogaotiantian/coredumpy/blob/master/NOTICE.txt
-
-import os
-
-
-if os.getenv('GITHUB_ACTIONS'):
-    try:
-        import coredumpy
-        coredumpy.patch_unittest(directory=os.getenv('COREDUMPY_DUMP_DIR', './dump'))
-    except ImportError:
-        pass
