5f9b2401802a317b1454cb428b70936dcd734584
Tian Gao <gaogaotiantian@hotmail.com>
2026-01-04 17:31:51 -0800

    Ready for 0.5.0

--- src/coredumpy/__init__.py
+++ src/coredumpy/__init__.py
@@ -2,7 +2,7 @@
 # For details: https://github.com/gaogaotiantian/coredumpy/blob/master/NOTICE.txt
 
 
-__version__ = "0.4.3"
+__version__ = "0.5.0"
 
 import coredumpy.pytest_hook as pytest_hook
 from .config import config
